5efb49df66661f11260603fee584aaeb242f672f
pub-art <pubblegg@gmail.com>
2026-05-04 19:46:17 +0800

    bind all local networks

--- webui/server.py
+++ webui/server.py
@@ -67,4 +67,4 @@ def create_app() -> FastAPI:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(create_app(), host="127.0.0.1", port=8765)
+    uvicorn.run(create_app(), host="0.0.0.0", port=8765)
